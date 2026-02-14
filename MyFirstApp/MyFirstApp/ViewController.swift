205d67dc3fa0049357a94fd29bf4d3c7b32f39c3
hojeong <parkhj062@gmail.com>
2026-02-14 20:19:33 +0900

    MyFirstApp init

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by hj on 2/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
