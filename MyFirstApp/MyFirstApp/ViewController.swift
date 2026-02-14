535cf8e1dabef1969cd32cd80e35f53ee16f9cc7
hojeong <parkhj062@gmail.com>
2026-02-14 22:01:37 +0900

    [앨런 Swift문법 마스터 스쿨 - 89. (앱 - 1: 기초) 앱 만들기 / 화면 / 오토레이아웃 설정하기 (66강)]

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLabel: UILabel!
+
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        mainLabel.text = "hello~~"
+        mainLabel.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        
+        myButton.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+    }
+    
 }
 
