f8939e36085fabb4818e3a2ab09c251a9c5cc406
parkhojeong <parkhj062@gmail.com>
2026-02-18 23:05:26 +0900

    [앨런 iOS 앱 개발 - 74. 여섯번째 앱 만들기 / 스토리보드로 네비게이션바/탭바 만들기 (앱 52강)]

---
+++ 6Nav&Tab/6Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6Nav&Tab
+//
+//  Created by hj on 2/18/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
